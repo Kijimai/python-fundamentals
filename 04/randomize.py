84bff27d2c1d36dc62838e6ad6fff1c6b1279ede
John Deen Bucog <JDBucog@yahoo.com>
2022-08-19 00:29:15 -0700

    ...

---
+++ 04/randomize.py
@@ -0,0 +1,13 @@
+import random
+
+# return a random number from a range of a to b
+rando = random.randint(0, 1)
+
+if (rando):
+  print("ayy", rando)
+else:
+  print("noooooo")   
+
+# Print a random float from 0 to 1 but not including 1
+rando_float = random.random()
+print(rando_float)
