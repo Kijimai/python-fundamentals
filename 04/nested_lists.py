592dc66f9f574ddbdae469b460b685688256c137
John Deen Bucog <JDBucog@yahoo.com>
2022-08-21 11:39:48 -0700

    treasure island

---
+++ 04/nested_lists.py
@@ -0,0 +1,4 @@
+fruits = ["strawberries", "apples", "oranges"]
+vegetables = ["eggplant", "lettuce", "kale"]
+dirty_dozen = [fruits, vegetables]
+print(dirty_dozen)
---
+++ 04/treasure_map.py
@@ -0,0 +1,12 @@
+row1 = ["⬜️","️⬜️","️⬜️"]
+row2 = ["⬜️","⬜️","️⬜️"]
+row3 = ["⬜️️","⬜️️","⬜️️"]
+
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("Where do you want to put the treasure? ")
+row_choice = int(position[0]) - 1
+index_choice = int(position[1]) - 1
+map[row_choice][index_choice] = "X"
+
+print(map)
