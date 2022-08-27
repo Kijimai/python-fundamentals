885531255a8ffe30147fcd2aa9dc712d082ecfb6
John Deen Bucog <JDBucog@yahoo.com>
2022-08-27 13:45:59 -0700

    blind auction

---
+++ 09/blind_auction.py
@@ -0,0 +1,32 @@
+from art import logo
+# from replit import clear
+# HINT: You can call clear() to clear the output in the console.
+
+print(logo)
+keep_going = True
+
+all_bidders = []
+
+
+def add_bidder(name, bid_amount):
+    print()
+    all_bidders.append({"bidder": name, "bid": bid_amount})
+    print(all_bidders)
+
+
+while keep_going:
+    name = input("What is the bidder's name? ")
+    bid_price = int(input("What is their bid? "))
+    add_bidder(name, bid_price)
+    answer = input("Bidder added! Keep going? type 'yes' or 'no'").lower()
+    if answer == "yes":
+        keep_going = True
+    else:
+        keep_going = False
+
+largest_bid = 0
+for bidder in all_bidders:
+    if bidder["bid"] > largest_bid:
+        largest_bid = bidder['bid']
+print(
+    f"The largest bid was {largest_bid}!")
