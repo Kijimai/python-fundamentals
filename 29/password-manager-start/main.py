4b92eff0d443d6d6ed5609e74772f603f773cfe5
John Deen Bucog <JDBucog@yahoo.com>
2022-09-16 17:59:40 -0700

    Password Generator -- installed pyperclip

--- 29/password-manager-start/main.py
+++ 29/password-manager-start/main.py
@@ -1,6 +1,7 @@
-from random import choice, randint
+from random import choice, randint, shuffle
 from tkinter import *
-;
+from tkinter import messagebox
+import pyperclip
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
            'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
@@ -9,7 +10,21 @@ symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 
 def generate_pass():
-    pass_entry.insert("")
+    pass_entry.delete(0, END)
+    generated = []
+
+    nr_letters = randint(8, 10)
+    nr_symbols = randint(2, 4)
+    nr_numbers = randint(2, 4)
+
+    generated += [choice(letters) for _ in range(nr_letters)]
+    generated += [choice(numbers) for _ in range(nr_numbers)]
+    generated += [choice(symbols) for _ in range(nr_symbols)]
+
+    shuffle(generated)
+    new_pass = ''.join(generated)
+    pyperclip.copy(new_pass)
+    pass_entry.insert(0, new_pass)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
@@ -19,7 +34,12 @@ def save_data():
     email_user_name = email_user_entry.get()
     password = pass_entry.get()
     if not web_name or not email_user_name or not password:
+        messagebox.showwarning(message="Please dont leave a field empty!")
         confirmation_label.config(text="Don't leave a field empty!", fg='red')
+        return
+    answer = messagebox.askquestion(
+        title="Save Data?", message=f"Are you sure you want to save this info?\nWebsite name: {web_name}\nEmail/Username: {email_user_name}\nPassword: {password}")
+    if answer == "no":
         return
     with open('./data.txt', mode="a") as data:
         data.writelines(f"{web_name} | {email_user_name} |{password}\n")
@@ -50,6 +70,7 @@ email_user_label.grid(row=2, column=0)
 pass_label.grid(row=3, column=0)
 confirmation_label = Label(font=("Arial", 16, "bold"), fg="green")
 confirmation_label.grid(column=1, row=5)
+
 # Buttons
 generate_btn = Button(text="Generate Password", command=generate_pass)
 add_btn = Button(text="Add", command=save_data)
