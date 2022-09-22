5cc141a655562241a6e162358a01cd5214c19400
John Deen Bucog <JDBucog@yahoo.com>
2022-09-22 00:58:51 -0700

    ..

--- 32/main.py
+++ 32/main.py
@@ -1,5 +1,25 @@
+import os
+from dotenv import load_dotenv
 import smtplib
-# Gmail: smtp.gmail.com
-# Hotmail: smtp.live.com
-# Outlook: outlook.office365.com
-# Yahoo: smtp.mail.yahoo.com
+
+load_dotenv()
+
+CURR_EMAIL = os.getenv('MY_EMAIL')
+CURR_PW = os.getenv('MY_PW')
+
+email_options = {
+    "yahoo": "smtp.mail.yahoo.com",
+    "hotmail": "smtp.live.com",
+    "outlook": "outlook.office365.com",
+    "gmail": "smtp.gmail.com"
+}
+
+
+# Take off port=587 options for non-gmail email
+# connection = smtplib.SMTP(email_options["gmail"], port=587)
+with smtplib.SMTP(email_options["gmail"], port=587) as connection:
+    # Starts a secure connection with the server
+    connection.starttls()
+    connection.login(user=CURR_EMAIL, password=CURR_PW)
+    connection.sendmail(from_addr=CURR_EMAIL,
+                        to_addrs="jdbucog@yahoo.com", msg="Subject:HElllooo Test\n\nThis is the body of my email!")
